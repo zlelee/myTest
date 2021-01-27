62b918c1a43b1cc299e4451b690b577703d42e41
lele <562282501@qq.com>
2021-01-27 19:37:25 +0800

    绘制最基础的盒子

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,17 +1,26 @@
 <template>
-  <div class="login_container"></div>
+  <div class="login_container">
+    <div class="login_box"></div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: '',
-
-  data() {
-    return {}
-  },
-
-  methods: {}
-}
+export default {}
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
