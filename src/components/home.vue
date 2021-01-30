611cbf574d454b6c3f1ce692f0c6c828d57a4ced
lele <562282501@qq.com>
2021-01-30 20:00:14 +0800

    退出功能

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,19 +1,39 @@
 <template>
-  <div>
-    home组件
-  </div>
+  <el-container class="home-container">
+    <el-header>Header<el-button type="info" @click="logout">退出</el-button> </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-  name: '',
-
+  name: 'Home',
   data() {
     return {}
   },
-
-  methods: {}
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+</style>
