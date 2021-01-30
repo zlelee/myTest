abc5c06352b7090bc0e1ef7b8617feb0b76ee41c
lele <562282501@qq.com>
2021-01-30 21:19:58 +0800

    侧边栏的折叠与炸开

--- src/components/home.vue
+++ src/components/home.vue
@@ -8,8 +8,9 @@
       <el-button type="info" @click="logout">退出</el-button>
     </el-header>
     <el-container>
-      <el-aside width="200px">
-        <el-menu default-active="2" background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse-transition="false">
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <el-menu default-active="2" background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse-transition="false" :collapse="isCollapse">
           <!-- 一级菜单 -->
           <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
             <template slot="title">
@@ -42,7 +43,8 @@ export default {
         101: 'iconfont icon-shangpin',
         102: 'iconfont icon-danju',
         145: 'iconfont icon-baobiao'
-      }
+      },
+      isCollapse: false
     }
   },
   methods: {
@@ -54,6 +56,9 @@ export default {
       const { data: res } = await this.$http.get('menus')
       if (res.meta.status !== 200) return this.$message.error('获取数据失败')
       this.menuList = res.data
+    },
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
     }
   },
   created() {
@@ -96,5 +101,14 @@ export default {
 }
 .iconfont {
   margin-right: 15px;
+}
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
 }
 </style>
